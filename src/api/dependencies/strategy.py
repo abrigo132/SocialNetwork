237fb1e2a21a5a29dbdbafed6a4c10470cbc9b30
danila <garipotter97@gmail.com>
2025-09-10 18:01:52 +0500

    add type checking for annotations

--- src/api/dependencies/strategy.py
+++ src/api/dependencies/strategy.py
@@ -1,23 +1,24 @@
 from fastapi import Depends
 from fastapi_users.authentication.strategy.db import (
-    AccessTokenDatabase,
     DatabaseStrategy,
 )
 from typing import TYPE_CHECKING, Annotated
 
 from core import settings
+from .access_tokens import get_access_tokens_db
 
 if TYPE_CHECKING:
-    from core.models import AccessToken, User
+    from core.models import AccessToken
+    from fastapi_users.authentication.strategy.db import AccessTokenDatabase
 
 
 def get_database_strategy(
     access_token_db: Annotated[
-        AccessTokenDatabase["AccessToken"],
-        Depends(get_access_token_db),
+        "AccessTokenDatabase[AccessToken]",
+        Depends(get_access_tokens_db),
     ],
 ) -> DatabaseStrategy:
     return DatabaseStrategy(
-        access_token_db,
+        database=access_token_db,
         lifetime_seconds=settings.access_token.lifetime_seconds,
     )
