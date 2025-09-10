4b3afbc791e9651f7997648d519096b4fbf76396
danila <garipotter97@gmail.com>
2025-09-10 14:31:18 +0500

    add custom fields for user model

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,8 +1,53 @@
+from typing import Optional
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy import (
+    String,
+    ARRAY,
+    Integer,
+    Text,
+    JSON,
+)
+from sqlalchemy.orm import Mapped, mapped_column
+from enum import Enum
+from datetime import datetime
 
 from .base import Base
 from core.models.mixins import IdIntPkMixin
 
 
+class PlayerRole(str, Enum):
+    CARRY = "Carry"
+    MIDLANER = "Midlaner"
+    OFFLANER = "Offlaner"
+    SOFT_SUPPORT = "Soft Support"
+    HARD_SUPPORT = "Hard Support"
+
+
 class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):
-    pass
+    username: Mapped[str] = mapped_column(
+        String(50), unique=True, index=True, nullable=False
+    )
+    display_name: Mapped[Optional[str]] = mapped_column(String(100), nullable=True)
+
+    # 2. Данные для связи с игрой
+    steam_id: Mapped[Optional[str]] = mapped_column(
+        String(20), unique=True, index=True, nullable=True
+    )
+    steam_profile_url: Mapped[Optional[str]] = mapped_column(String(200), nullable=True)
+
+    # 3. Игровая информация
+    main_role: Mapped[Optional[PlayerRole]] = mapped_column(nullable=True)
+    secondary_roles: Mapped[Optional[list[PlayerRole]]] = mapped_column(
+        ARRAY(String), nullable=True
+    )
+    current_mmr: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
+    peak_mmr: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
+
+    # 4. Социальная составляющая
+    bio: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
+    country: Mapped[Optional[str]] = mapped_column(String(100), nullable=True)
+    social_links: Mapped[Optional[dict]] = mapped_column(JSON, nullable=True)
+    avatar_url: Mapped[Optional[str]] = mapped_column(String(200), nullable=True)
+
+    # Служебные поля для обновления данных
+    last_stats_update: Mapped[Optional[datetime]] = mapped_column(nullable=True)
