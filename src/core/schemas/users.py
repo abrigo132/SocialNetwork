72075c8090f7e89bb31981c0412ae7f799d70f62
danila <garipotter97@gmail.com>
2025-09-16 14:17:16 +0500

    create user base schema

--- src/core/schemas/users.py
+++ src/core/schemas/users.py
@@ -5,7 +5,7 @@ from core.models.user import PlayerRole
 from core.types import UserIdType
 
 
-class UserRead(schemas.BaseUser[UserIdType]):
+class UserBase(schemas.BaseUser[UserIdType]):
     username: str
     display_name: str
     steam_id: str
@@ -19,15 +19,19 @@ class UserRead(schemas.BaseUser[UserIdType]):
     avatar_url: Optional[str]
 
 
+class UserRead(UserBase):
+    pass
+
+
 class UserCreate(schemas.BaseUserCreate):
     pass
 
 
 class UserUpdate(schemas.BaseUserUpdate):
-    username: str
-    display_name: str
-    steam_id: str
-    steam_profile_url: str
+    username: Optional[str]
+    display_name: Optional[str]
+    steam_id: Optional[str]
+    steam_profile_url: Optional[str]
     main_role: Optional[PlayerRole]
     current_mmr: Optional[str]
     peak_mmr: Optional[str]
