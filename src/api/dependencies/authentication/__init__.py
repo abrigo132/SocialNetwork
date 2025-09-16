6cb992c665b84f4a2f5abd11d5acfc1d1f3e241e
danila <garipotter97@gmail.com>
2025-09-16 15:24:58 +0500

    create auth fastapi users routers

---
+++ src/api/dependencies/authentication/__init__.py
@@ -0,0 +1,7 @@
+__all__ = (
+    "fastapi_users",
+    "authentication_backend",
+)
+
+from .fastapi_users_routers import fastapi_users
+from .backend import authentication_backend
--- src/api/v1/auth.py
+++ src/api/v1/auth.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 
 from core import settings
-
+from api.dependencies.authentication import fastapi_users
+from api.dependencies.authentication import authentication_backend
 
 router = APIRouter(prefix=settings.api.v1.auth, tags=["Authentication"])
+
+
+router.include_router(router=fastapi_users.get_auth_router(authentication_backend))
